c99ac03b78ce2b86bed7bd0ba2874fefcf31aea2
Pozhidaev <pozidaev2002@gmail.com>
2022-04-16 19:30:20 +0500

    Sanamen102 доработалт класс са1с

--- src/main/java/circle/Calc.java
+++ src/main/java/circle/Calc.java
@@ -1,8 +1,12 @@
 package circle;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name="Calc", urlPatterns="/Result")
 
@@ -10,7 +14,7 @@ public class Calc extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestCalc Calc = RequestCalc.fromRequestParameters(request);
-		Calc.setAsRequestAttributesAndCalcuklate(request);
+		Calc.setAsRequestAttributesAndCalculate(request);
 		
 		request.getRequestDispatcher("/Results.jsp").forward(request, response);
 	}
